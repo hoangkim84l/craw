018cbb939029d5ca230991270d2ce3b19246553e
Duy Vo <duy.vo@manifera.com>
2023-12-07 11:09:29 +0700

    Update format xml

--- app/Console/Commands/General/UpdateXmlSiteMap.php
+++ app/Console/Commands/General/UpdateXmlSiteMap.php
@@ -32,11 +32,8 @@ class UpdateXmlSiteMap extends Command
     public function handle()
     {
         $data = '<?xml version="1.0" encoding="UTF-8"?>
-                <urlset>
-                    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+                <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
                 <!-- created with Free Online Sitemap Generator www.cafesuanovel.com -->
 
             <url>
